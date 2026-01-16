b920fa124a6774ff15780041690bba92a2bf8465
Lucas Roig <lucas1.roig@gmail.com>
2026-01-16 17:01:57 +0100

    feat: ask project path

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-import { intro, outro } from "@clack/prompts";
+import { intro, outro, log, text } from "@clack/prompts";
+import { resolve } from "node:path";
 import color from "picocolors";
 
-function main() {
+async function main() {
+  const cwd = process.cwd();
   intro(color.inverse("Mount stack"));
+  log.info(`Current directory ${cwd}`)
+  const relativeProjectPath = await text({
+    message: "Where would you like to create your project ?",
+    defaultValue: "./my-app",
+    placeholder: "./my-app",
+    validate: (value: string) => {
+      if (!value.startsWith(".")) {
+        return "Relative path should start with a dot ."
+      }
+      try {
+        resolve(cwd, value)
+      } catch (e) {
+        return "Invalid path"
+      }
+    }
+  })
+  const projectPath = resolve(cwd, relativeProjectPath as string)
+  log.info(`Project path ${projectPath}`)
   outro("Done !");
 }
 
