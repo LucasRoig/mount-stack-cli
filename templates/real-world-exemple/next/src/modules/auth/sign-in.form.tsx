d270dca661f4a17cb8811cd925bf366a9b3345d6
Lucas Roig <lucas1.roig@gmail.com>
2026-04-08 17:05:41 +0200

    feat(real-world): sign-up page wip

--- templates/real-world-exemple/next/src/modules/auth/sign-in.form.tsx
+++ templates/real-world-exemple/next/src/modules/auth/sign-in.form.tsx
@@ -5,32 +5,23 @@ import { useAppForm } from "@lro-ui/form";
 import { useRef } from "react";
 import z from "zod";
 
-//TODO: Make a custom password field with strenght indicator to show to extend the app form declared in the design system with new components.
 
-const signUpFormSchema = z
+const signInFormSchema = z
   .object({
-    username: z.string().min(3),
     email: z.email(),
     password: z.string().min(8),
-    confirmPassword: z.string().min(8),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords don't match",
-    path: ["confirmPassword"],
   });
 
 const formDefaultValues = {
-  username: "",
   email: "",
   password: "",
-  confirmPassword: "",
 };
 
-export function SignUpForm() {
+export function SignInForm() {
   const form = useAppForm({
     defaultValues: formDefaultValues,
     validators: {
-      onSubmit: signUpFormSchema,
+      onSubmit: signInFormSchema,
     },
   });
   const formRef = useRef<HTMLFormElement>(null);
@@ -39,15 +30,11 @@ export function SignUpForm() {
     <form.AppForm>
       <form.FormRoot ref={formRef}>
         <FieldGroup>
-          <form.AppField name="username">{(field) => <field.TextField label="Username" />}</form.AppField>
           <form.AppField name="email">{(field) => <field.TextField label="Email" />}</form.AppField>
           <form.AppField name="password">
-            {(field) => <field.TextField label="Password" description="Must be at least 8 characters long." />}
+            {(field) => <field.TextField label="Password" />}
           </form.AppField>
-          <form.AppField name="confirmPassword">
-            {(field) => <field.TextField label="Confirm Password" description="Please confirm your password." />}
-          </form.AppField>
-          <form.SubmitButton>Create your account</form.SubmitButton>
+          <form.SubmitButton>Sign In</form.SubmitButton>
           <FieldSeparator>Or</FieldSeparator>
           <div className="flex flex-col gap-4">
             <Button variant="secondary" type="button">Continue with OIDC</Button>
---
+++ templates/real-world-exemple/next/src/modules/auth/sign-in.page.tsx
@@ -0,0 +1,209 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { Container } from "@/components/layout/container";
+import { getAuth } from "@/lib/auth/auth";
+// import { type $ERROR_CODES, signIn, signUp } from "@/lib/auth/auth-client";
+import { SignInForm } from "./sign-in.form";
+
+// type ErrorTypes = Partial<Record<keyof typeof $ERROR_CODES, string>>;
+
+//TODO: On rencontre une erreur à travers le provider OIDC lorsque l'utilisateur s'est déjà connecté une fois avec un autre provider
+//L'utilisateur est redirigé vers /?error=account_not_linked sans la moindre erreur ou message.
+
+// const useGetAuthErrorMessage = () => {
+//   const t = useTranslations("authErrors");
+//   const errorCodes = {
+//     USER_ALREADY_EXISTS: t("USER_ALREADY_EXISTS"),
+//     INVALID_EMAIL_OR_PASSWORD: t("INVALID_EMAIL_OR_PASSWORD"),
+//   } satisfies ErrorTypes;
+//   return useCallback(
+//     (code: string) => {
+//       if (code in errorCodes) {
+//         return errorCodes[code as keyof typeof errorCodes];
+//       }
+//       return `Unknown error with code ${code}`;
+//     },
+//     [errorCodes],
+//   );
+// };
+
+export async function SigninPage() {
+  const session = await getAuth().api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    return redirect("/");
+  }
+  return (
+    <Container className="mt-6 w-full px-4" maxWidth="sm">
+      <div className="flex flex-col items-center gap-1 text-center">
+        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <p className="text-sm text-balance text-muted-foreground">Enter your email below to login to your account</p>
+      </div>
+      <div className="mt-6 w-full">
+        <SignInForm />
+      </div>
+    </Container>
+  );
+}
+
+// function SignInBlock() {
+//   const getErrorMessage = useGetAuthErrorMessage();
+//   const router = useRouter();
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     const { data: _data, error } = await signIn.email({
+//       email: email,
+//       password: password,
+//     });
+//     if (error?.code) {
+//       alert(getErrorMessage(error.code));
+//     } else {
+//       router.push("/");
+//     }
+//   };
+//   const handleSSOLogin = async (args: { providerId: string; providerKind: "oidc" | "saml"; forceLogin?: boolean }) => {
+//     const { error } = await signIn.sso({
+//       providerId: args.providerId,
+//       callbackURL: "/",
+//       errorCallbackURL: "/sign-in",
+//       newUserCallbackURL: "/bliblu",
+//       fetchOptions: {
+//         query: {
+//           forceLogin: args.forceLogin ? "true" : "false",
+//           providerKind: args.providerKind,
+//         },
+//       },
+//     });
+//     if (error?.code) {
+//       alert(getErrorMessage(error.code));
+//     } else {
+//       router.push("/");
+//     }
+//   };
+//   const handleOIDCLogin = (providerId: string) =>
+//     handleSSOLogin({ providerId, providerKind: "oidc", forceLogin: true });
+//   const handleSAMLLogin = (providerId: string) =>
+//     handleSSOLogin({ providerId, providerKind: "saml", forceLogin: true });
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <div className="flex gap-2">
+//           <label htmlFor="email">Email</label>
+//           <input
+//             type="email"
+//             name="email"
+//             className="border"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//           />
+//         </div>
+//         <div className="flex gap-2">
+//           <label htmlFor="password">Password</label>
+//           <input
+//             type="password"
+//             name="password"
+//             className="border"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//         </div>
+//         <button className="border px-2" type="submit">
+//           Submit
+//         </button>
+//       </form>
+//       <button className="border px-2" type="button" onClick={() => handleOIDCLogin("local-oidc")}>
+//         Signin with OIDC
+//       </button>
+//       <button className="border px-2" type="button" onClick={() => handleSAMLLogin("local-saml")}>
+//         Signin with SAML
+//       </button>
+//     </div>
+//   );
+// }
+// function SignUpBlock() {
+//   const signUpMutation = useMutation({
+//     mutationFn: async (form: { firstName: string; lastName: string; email: string; password: string }) => {
+//       const { data, error } = await signUp.email({
+//         name: `${form.firstName} ${form.lastName}`,
+//         email: form.email,
+//         password: form.password,
+//       });
+//       if (error) {
+//         throw new Error(JSON.stringify(error));
+//       }
+//       return data;
+//     },
+//   });
+//   const [firstName, setFirstName] = useState("");
+//   const [lastName, setLastName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [confirmPassword, setConfirmPassword] = useState("");
+
+//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     signUpMutation.mutate(
+//       { firstName, lastName, email, password },
+//       {
+//         onSuccess: () => alert("User created successfully"),
+//         onError: (err) => alert(`Error while creating user : ${err instanceof Error ? err.message : "Unknown error"}`),
+//       },
+//     );
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div className="flex gap-2">
+//         <label htmlFor="name">First Name</label>
+//         <input
+//           type="text"
+//           name="name"
+//           className="border"
+//           value={firstName}
+//           onChange={(e) => setFirstName(e.target.value)}
+//         />
+//       </div>
+//       <div className="flex gap-2">
+//         <label htmlFor="name">Last Name</label>
+//         <input
+//           type="text"
+//           name="name"
+//           className="border"
+//           value={lastName}
+//           onChange={(e) => setLastName(e.target.value)}
+//         />
+//       </div>
+//       <div className="flex gap-2">
+//         <label htmlFor="email">Email</label>
+//         <input type="email" name="email" className="border" value={email} onChange={(e) => setEmail(e.target.value)} />
+//       </div>
+//       <div className="flex gap-2">
+//         <label htmlFor="password">Password</label>
+//         <input
+//           type="password"
+//           name="password"
+//           className="border"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//       </div>
+//       <div className="flex gap-2">
+//         <label htmlFor="confirmPassword">Confirm Password</label>
+//         <input
+//           type="password"
+//           name="confirmPassword"
+//           className="border"
+//           value={confirmPassword}
+//           onChange={(e) => setConfirmPassword(e.target.value)}
+//         />
+//       </div>
+//       <button className="border px-2" type="submit">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// }
