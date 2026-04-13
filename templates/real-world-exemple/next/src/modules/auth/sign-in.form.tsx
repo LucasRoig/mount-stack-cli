b91dc67c7223790f1dc72dba3f158414ea3639b4
Lucas Roig <lucas1.roig@gmail.com>
2026-04-13 10:09:18 +0200

    feat(real-world): sign in

--- templates/real-world-exemple/next/src/modules/auth/sign-in.form.tsx
+++ templates/real-world-exemple/next/src/modules/auth/sign-in.form.tsx
@@ -2,15 +2,17 @@
 import { Button } from "@lro-ui/button";
 import { FieldGroup, FieldSeparator } from "@lro-ui/field";
 import { useAppForm } from "@lro-ui/form";
-import { useRef } from "react";
+import { toast } from "@lro-ui/sonner";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
 import z from "zod";
+import { AuthErrorField, type AuthError } from "./auth-error-field";
+import { signIn } from "@/lib/auth/auth-client";
 
-
-const signInFormSchema = z
-  .object({
-    email: z.email(),
-    password: z.string().min(8),
-  });
+const signInFormSchema = z.object({
+  email: z.email(),
+  password: z.string().nonempty("Please enter your password"),
+});
 
 const formDefaultValues = {
   email: "",
@@ -18,10 +20,27 @@ const formDefaultValues = {
 };
 
 export function SignInForm() {
+  const router = useRouter();
+  const [globalError, setGlobalError] = useState<AuthError>();
   const form = useAppForm({
     defaultValues: formDefaultValues,
     validators: {
       onSubmit: signInFormSchema,
+    },
+    onSubmit: async ({ value }) => {
+      setGlobalError(undefined);
+      const { error } = await signIn.email({
+        email: value.email,
+        password: value.password
+      });
+      if (error) {
+        console.log(error);
+        setGlobalError(error);
+      } else {
+        router.push("/");
+        router.refresh();
+        toast.success("You have signed in successfully!");
+      }
     },
   });
   const formRef = useRef<HTMLFormElement>(null);
@@ -31,14 +50,17 @@ export function SignInForm() {
       <form.FormRoot ref={formRef}>
         <FieldGroup>
           <form.AppField name="email">{(field) => <field.TextField label="Email" />}</form.AppField>
-          <form.AppField name="password">
-            {(field) => <field.TextField label="Password" />}
-          </form.AppField>
+          <form.AppField name="password">{(field) => <field.TextField label="Password" type="password" />}</form.AppField>
+          <AuthErrorField error={globalError} />
           <form.SubmitButton>Sign In</form.SubmitButton>
           <FieldSeparator>Or</FieldSeparator>
           <div className="flex flex-col gap-4">
-            <Button variant="secondary" type="button">Continue with OIDC</Button>
-            <Button variant="secondary" type="button">Continue with SAML</Button>
+            <Button variant="secondary" type="button">
+              Continue with OIDC
+            </Button>
+            <Button variant="secondary" type="button">
+              Continue with SAML
+            </Button>
           </div>
         </FieldGroup>
       </form.FormRoot>
