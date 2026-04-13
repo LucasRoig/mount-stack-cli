fee630ce781c59de53349fd4e548b40e5a68a982
Lucas Roig <lucas1.roig@gmail.com>
2026-04-13 10:47:07 +0200

    feat(real-world): profile and settings page

--- templates/real-world-exemple/next/src/modules/profile/profile.page.tsx
+++ templates/real-world-exemple/next/src/modules/profile/profile.page.tsx
@@ -1,4 +1,57 @@
+import { Button } from "@lro-ui/button";
+import { UserRoundPenIcon } from "lucide-react";
+import { headers } from "next/headers";
+import Image from "next/image";
+import Link from "next/link";
+import { Container } from "@/components/layout/container";
+import { getAuth } from "@/lib/auth/auth";
+
 export async function ProfilePage(props: PageProps<"/profile/[username]">) {
   const { username } = await props.params;
-  return <div>Profile page {username}</div>;
+  const session = await getAuth().api.getSession({
+    headers: await headers(),
+  });
+  const isMe = session?.user.name === username;
+
+  return (
+    <div>
+      <ProfilePageHero username={username} isMe={isMe} />
+      <Feed />
+    </div>
+  );
+}
+
+function ProfilePageHero(props: { username: string; isMe: boolean }) {
+  return (
+    <div className="border-b p-8 pb-4 mb-8">
+      <Container className="text-center">
+        <Image
+          src="/default-avatar.svg"
+          alt="user avatar"
+          width={100}
+          height={100}
+          className="rounded-full mx-auto mb-4"
+        />
+        <h1 className="text-2xl font-bold mb-2">{props.username}</h1>
+        {props.isMe && (
+          <div className="text-right">
+            <Button variant="secondary" size="xs" asChild>
+              <Link href="/settings">
+                <UserRoundPenIcon />
+                Edit Profile
+              </Link>
+            </Button>
+          </div>
+        )}
+      </Container>
+    </div>
+  );
+}
+
+function Feed() {
+  return (
+    <Container>
+      <div className="bg-red-300">Feed</div>
+    </Container>
+  );
 }
---
+++ templates/real-world-exemple/next/src/modules/settings/settings.page.tsx
@@ -0,0 +1,7 @@
+export function SettingsPage(_props: PageProps<"/settings">) {
+  return (
+    <div>
+      <h1>Settings</h1>
+    </div>
+  );
+}
